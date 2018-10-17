893e89bd90b032362b35df0f36a00670384c738d
Jonathan Luk <jluk87@gmail.com>
2018-10-17 01:11:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     @number_phrase = @number_of_times.times do
       @phrase += " #{@phrase}"
     end
-    
+    "#{@phrase}"
   end
   # get '/say/:word1/:word2/:word3/:word4/:word5' do
   #
