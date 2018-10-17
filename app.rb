315d7077dc52ae197205e8c84c3b7a725e9db84e
Jonathan Luk <jluk87@gmail.com>
2018-10-17 01:01:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,11 +17,11 @@ class App < Sinatra::Base
       puts "#{@phrase}"
     end
   end
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-
-  end
-  get '/:operation/:number1/:number2' do
-
-  end
+  # get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #
+  # end
+  # get '/:operation/:number1/:number2' do
+  #
+  # end
 
 end
