5bea11ab4e0fc0c94edfa81e0d65d3951c5a87f9
Jonathan Luk <jluk87@gmail.com>
2018-10-17 01:04:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,10 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number_of_times = params[:number].to_i
     @phrase = params[:phrase]
-    @number_of_times.times do
+    # @number_of_times.times do
       "#{@phrase}"
-    end
-    
+    # end
+
   end
   # get '/say/:word1/:word2/:word3/:word4/:word5' do
   #
